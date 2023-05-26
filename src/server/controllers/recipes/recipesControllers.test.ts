496c8017d8942a857d17b6eca6dc83ac18cd4ce2
Luis <hola@luiscaballe.com>
2023-05-26 23:16:39 +0200

    Test getRecipes controller

---
+++ src/server/controllers/recipes/recipesControllers.test.ts
@@ -0,0 +1,52 @@
+import { type NextFunction, type Request, type Response } from "express";
+import { getRecipes } from "./recipesControllers";
+import Recipe from "../../../database/models/Recipe";
+import { mockRecipes } from "../../../data/recipes";
+import { correctResponse } from "../../utils/responseData/responseData";
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+describe("Given a getRecipes controller", () => {
+  const req = {};
+  const res: Partial<Response> = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  };
+  const next = jest.fn();
+
+  describe("When it receives a response", () => {
+    Recipe.find = jest.fn().mockReturnValue({
+      limit: jest.fn().mockReturnThis(),
+      exec: jest.fn().mockResolvedValue(mockRecipes),
+    });
+    test("Then it should call the response's method status with 200", async () => {
+      const expectedStatus = correctResponse.statusCode;
+
+      await getRecipes(req as Request, res as Response, next as NextFunction);
+
+      expect(res.status).toHaveBeenCalledWith(expectedStatus);
+    });
+
+    test("Then it should call the response's method json with a list of recipes", async () => {
+      await getRecipes(req as Request, res as Response, next as NextFunction);
+
+      expect(res.json).toHaveBeenCalledWith({ recipes: mockRecipes });
+    });
+  });
+  describe("When it receives a next function and the exec method rejects with an 'tot malament' error", () => {
+    test("Then it should call next function with the error 'tot malament'", async () => {
+      const expectedError = new Error("Error connecting to database");
+
+      Recipe.find = jest.fn().mockReturnValue({
+        limit: jest.fn().mockReturnThis(),
+        exec: jest.fn().mockRejectedValue(expectedError),
+      });
+
+      await getRecipes(req as Request, res as Response, next as NextFunction);
+
+      expect(next).toHaveBeenCalledWith(expectedError);
+    });
+  });
+});
--- src/server/controllers/recipes/recipesControllers.ts
+++ src/server/controllers/recipes/recipesControllers.ts
@@ -1,9 +1,13 @@
 import { type NextFunction, type Request, type Response } from "express";
-import Recipe from "../../../database/models/Recipe";
+import Recipe from "../../../database/models/Recipe.js";
 
-export const getRecipes = (req: Request, res: Response, next: NextFunction) => {
+export const getRecipes = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
-    const recipes = Recipe.find().limit(10).exec();
+    const recipes = await Recipe.find().limit(10).exec();
 
     res.status(200).json({ recipes });
   } catch (error) {
