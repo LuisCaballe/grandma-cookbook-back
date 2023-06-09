a2d29ba81362f11e241db4b3b523d92cde33848c
Luis <hola@luiscaballe.com>
2023-06-09 15:26:51 +0200

    Add addRecipe controller in recipeRouter

--- src/server/routers/recipe/recipeRouter.ts
+++ src/server/routers/recipe/recipeRouter.ts
@@ -1,14 +1,24 @@
 import { Router } from "express";
 import auth from "../../middlewares/authMiddleware/authMiddleware.js";
 import {
+  addRecipe,
   getRecipes,
   removeRecipe,
 } from "../../controllers/recipes/recipesControllers.js";
+import { validate } from "express-validation";
+import recipeSchema from "../../../schemas/recipeSchema.js";
 
 const recipeRouter = Router();
 
 recipeRouter.get("/", auth, getRecipes);
 
 recipeRouter.delete("/:recipeId", auth, removeRecipe);
+
+recipeRouter.post(
+  "/add",
+  auth,
+  validate(recipeSchema, {}, { abortEarly: false }),
+  addRecipe
+);
 
 export default recipeRouter;
